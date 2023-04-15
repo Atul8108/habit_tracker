2e534905e5ea73f02b53134bdbdbd288f23fa9b2
Atul Shaw <shawatul4@gmail.com>
2023-04-16 01:49:04 +0530

    adding mongodb deploying it

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://127.0.0.1/todo_list_db');
+mongoose.connect('mongodb+srv://shawatul4:shawatul4@cluster0.doflsji.mongodb.net/habit-tracker?retryWrites=true&w=majority');
 const db = mongoose.connection;
 // error
-db.on('error',console.error.bind(console,'erroe connecting to db'));
+db.on('error', console.error.bind(console, 'erroe connecting to db'));
 // up and running then message
-db.once('open',function(){
+db.once('open', function () {
     console.log('Success fully connected to the database')
 })
